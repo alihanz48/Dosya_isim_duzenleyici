94823a0375c7ff695a10d7ac279abc25c1eddc0a
alihanz48 <dursunalihan@gmail.com>
2023-02-15 18:39:13 +0300

    Dosya isim düzenleyici
    Dosyalarınızın ismini toplu bir şekilde düzenlemeye yarayan bir programdır.Dosyaların isimlerinin tümünü büyük/küçük harf yapmaya,numaralandırmaya,yalnızca baş harfleri büyük harf yapmaya,uzantı değiştirmeye yarar.

---
+++ Dosya isim düzenleyici/Dosya isim düzenleyici/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Dosya_isim_düzenleyici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(21, 171);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(87, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Numaralandır";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(21, 195);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(133, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hepsini büyük harf yap";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.Color.White;
+            this.radioButton3.Location = new System.Drawing.Point(21, 218);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(134, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Hepsini küçük harf yap";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.ForeColor = System.Drawing.Color.White;
+            this.radioButton4.Location = new System.Drawing.Point(21, 241);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(129, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Baş harfleri büyük yap";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.ForeColor = System.Drawing.Color.White;
+            this.radioButton5.Location = new System.Drawing.Point(21, 264);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(91, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Uzantı değiştir";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(44, 285);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Yeni uzantı :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 282);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(233, 474);
+            this.panel1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Dosyaları al";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(21, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Uygula";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(251, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(554, 147);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(251, 264);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(554, 147);
+            this.listBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(254, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Eski :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(254, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Yeni :";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(817, 474);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Dosya isim düzenleyici/Dosya isim düzenleyici/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Dosya_isim_düzenleyici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string yol = "";
+        string dsyad = "";
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                yol = folderBrowserDialog1.SelectedPath;
+                
+                foreach (string dsyuzad in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                {
+                    listBox1.Items.Add(dsyuzad.Substring(yol.Length + 1));
+                }
+            }
+            dsyuzanti();
+        }
+
+
+        string uzanti = "";
+        private void dsyuzanti()
+        {
+            int nindex = 0;
+            //Uzantıyı elde etmek için döngü
+            foreach (String dsyisim in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+            {
+                for (int j = dsyisim.Length - 1; j >= 0; j--)
+                {
+                    if (dsyisim[j] == '.')
+                    {
+                        nindex = j;
+                        break;
+                    }
+                }
+                uzanti = dsyisim.Substring(nindex);
+                break;
+            }
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true|| radioButton2.Checked == true|| radioButton3.Checked == true|| radioButton4.Checked == true|| radioButton5.Checked == true)
+            {
+                listBox2.Items.Clear();
+                if (radioButton1.Checked == true)
+                {
+                    int i = 1;
+                    foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        dsyuzanti();
+                        File.Move(item, yol + "\\" + i.ToString() + uzanti);
+                        i++;
+                    }
+                }
+                else if (radioButton2.Checked == true)
+                {
+
+                    foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        dsyad = item.Substring(yol.Length + 1);
+                        File.Move(item, yol + "\\" + dsyad.ToUpper());
+                    }
+                }
+                else if (radioButton3.Checked == true)
+                {
+                    foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        dsyad = item.Substring(yol.Length + 1);
+                        File.Move(item, yol + "\\" + dsyad.ToLower());
+                    }
+                }
+                else if (radioButton4.Checked == true)
+                {
+                    foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        dsyad = item.Substring(yol.Length + 1);
+
+                        for (int i = 0; i < dsyad.Length; i++)
+                        {
+                            if (i == 0)
+                            {
+                                string ihrf = dsyad[0].ToString().ToUpper();
+                                dsyad = dsyad.Remove(0, 1);
+                                dsyad = ihrf + dsyad;
+                            }
+                            if (dsyad[i] == ' ')
+                            {
+                                string byk = dsyad[i + 1].ToString().ToUpper();
+                                dsyad = dsyad.Remove(i + 1, 1);
+                                dsyad = dsyad.Insert(i + 1, byk);
+                            }
+                        }
+                        File.Move(item, yol + "\\" + dsyad);
+                    }
+                }
+                else if (radioButton5.Checked == true)
+                {
+                    foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                    {
+                        dsyuzanti();
+                        dsyad = item.Substring(yol.Length + 1);
+                        dsyad = dsyad.Substring(0, dsyad.Length - uzanti.Length);
+                        dsyad = dsyad + "." + textBox1.Text;
+                        File.Move(item, yol + "\\" + dsyad);
+                    }
+                }
+                foreach (string item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+                {
+                    listBox2.Items.Add(item.Substring(yol.Length + 1));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir eylem seçiniz");
+            }
+            
+        }
+    }
+}
